b49c94bc2d3a94575bfa15a2a442ba9ff6ce5883
Raffael Schmid <raffael@yux.ch>
2016-09-28 00:48:38 +0200

    dev setup with nginx and dnsmasq

---
+++ setup/dev-setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+mkdir -p /etc/resolver
+echo "nameserver 127.0.0.1" > /etc/resolver/dev
+cp ./files/dnsmasq.conf /usr/local/etc/dnsmasq.conf
+sudo brew services start dnsmasq
+
+sed -E -e 's/listen( +)8080;/listen\180;/' -i '' /usr/local/etc/nginx/nginx.conf
+sudo brew services start nginx
