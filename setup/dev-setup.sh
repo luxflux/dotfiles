e09642850f58bd2c308a206c52e58ab06f71d8e5
Raffael Schmid <raffael@yux.ch>
2018-03-22 21:27:18 +0100

    redirect http and https

--- setup/dev-setup.sh
+++ setup/dev-setup.sh
@@ -15,6 +15,12 @@ mkdir -p /usr/local/var/log/caddy
 sed "s/::DNSIMPLE_EMAIL::/${DNSIMPLE_EMAIL}/; s/::DNSIMPLE_TOKEN::/${DNSIMPLE_TOKEN}/" ./files/com.caddyserver.web.plist > ~/Library/LaunchAgents/com.caddyserver.web.plist
 launchctl load -w ~/Library/LaunchAgents/com.caddyserver.web.plist
 
+echo "Redirect http and https to caddy"
+sudo cp ./files/pf-anchors-caddy /etc/pf.anchors/caddy
+sudo cp ./files/pf-caddy /etc/pf-caddy.conf
+sudo cp ./files/com.apple.pfctl-caddy.plist /Library/LaunchDaemons/com.apple.pfctl-caddy.plist
+sudo launchctl load -w /Library/LaunchDaemons/com.apple.pfctl-caddy.plist
+
 echo "Adding zsh to shells"
 echo /usr/local/bin/zsh | sudo tee -a /etc/shells
 echo "Changing shell for raf"
