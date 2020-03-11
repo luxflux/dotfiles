4ae4408c539d8edb540cd010f7a8dd2074de23bb
Raffael Schmid <raffael@yux.ch>
2020-03-11 11:38:54 -0600

    add nodenv to fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,7 @@
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
+status --is-interactive; and source (nodenv init -|psub)
+
 export HTTPS_DEVELOPMENT=true
 
 export EDITOR="nvim"
