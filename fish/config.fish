b2556180e3ed1d0b438c1ed29d748d3d7da9dd5f
Raffael Schmid <raffael@yux.ch>
2020-07-29 10:21:16 +0200

    updates

--- fish/config.fish
+++ fish/config.fish
@@ -3,6 +3,7 @@ test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shel
 status --is-interactive; and source (nodenv init -|psub)
 
 export HTTPS_DEVELOPMENT=true
+export WEBPACKER_DEV_SERVER_HTTPS=true
 
 export EDITOR="nvim"
 export GIT_EDITOR=$EDITOR
@@ -10,3 +11,4 @@ export VISUAL=$EDITOR
 export FZF_DEFAULT_COMMAND='rg --files --hidden --glob \!.git'
 
 test -e {$HOME}/.local_config.fish ; and source {$HOME}/.local_config.fish
+set -g fish_user_paths "/usr/local/opt/terraform@0.11/bin" $fish_user_paths
---
+++ fish/functions/oni2.fish
@@ -0,0 +1,3 @@
+function oni2
+  /Applications/Onivim2.app/Contents/MacOS/Oni2 $argv
+end
